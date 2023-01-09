38c3d53515ecfa4125182eb409f954de1f6edcd7
Mikhail Snytkov <coolfilinz@gmail.com>
2023-01-09 16:43:55 +0300

    updated haxe

--- haxe/haxe/bin/flash/haxe/ManifestResources.hx
+++ haxe/haxe/bin/flash/haxe/ManifestResources.hx
@@ -1,6 +1,5 @@
 package;
 
-
 import haxe.io.Bytes;
 import lime.utils.AssetBundle;
 import lime.utils.AssetLibrary;
@@ -11,6 +10,18 @@ import lime.utils.Assets;
 import sys.FileSystem;
 #end
 
+#if disable_preloader_assets
+@:dox(hide) class ManifestResources {
+	public static var preloadLibraries:Array<Dynamic>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+	public static function init (config:Dynamic):Void {
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+	}
+}
+#else
 @:access(lime.utils.Assets)
 
 
@@ -66,15 +77,8 @@ import sys.FileSystem;
 
 		#else
 
-		data = '{"name":null,"assets":"ah","rootPath":null,"version":2,"libraryArgs":[],"libraryType":null}';
-		manifest = AssetManifest.parse (data, rootPath);
-		library = AssetLibrary.fromManifest (manifest);
-		Assets.registerLibrary ("default", library);
 		
 
-		library = Assets.getLibrary ("default");
-		if (library != null) preloadLibraries.push (library);
-		else preloadLibraryNames.push ("default");
 		
 
 		#end
@@ -94,12 +98,10 @@ null
 #if !display
 #if flash
 
-@:keep @:bind @:noCompletion #if display private #end class __ASSET__manifest_default_json extends flash.utils.ByteArray { }
 
 
 #elseif (desktop || cpp)
 
-@:keep @:file("") @:noCompletion #if display private #end class __ASSET__manifest_default_json extends haxe.io.Bytes {}
 
 
 
@@ -118,6 +120,8 @@ null
 #end
 
 #end
+#end
+
 #end
 
 #end
--- haxe/haxe/src/Main.hx
+++ haxe/haxe/src/Main.hx
@@ -7,17 +7,87 @@ import openfl.Lib;
  * ...
  * @author filinz
  */
-class Main extends Sprite 
-{
+class Main extends Sprite {
 
-	public function new() 
-	{
+	public function new() {
 		super();
-		
-		trace("ok");
-		
-		// Assets:
-		// openfl.Assets.getBitmapData("img/assetname.jpg");
-	}
 
+		// HAXE
+
+		// ЛЕКСИЧЕСКАЯ СТРУКТУРА
+		/*
+		Переменные чувствительны к регистру
+		Пробелы и переводы строк игнорируются
+		Индентификаторы начинаются с буквы/_/$, далее буквы/цифры/_/$
+		Автономную единицу представляет блок - отрезок программного кода в фигурных скобках, всегда возвращает последнее вычисленное значение
+		*/
+
+		//Sys.println("now Hello World!");
+
+		// ТИПЫ ДАННЫХ, ЗНАЧЕНИЯ И ПЕРЕМЕННЫЕ
+		// Простые типы
+		var myInt:Int = 100; // совместим с Float, но не наоборот
+		var myFloat:Float = 10.45;
+		var myString:String = "строка";
+		var myBool:Bool = true;
+		// ereg
+		// Null
+		// Void - результат отсутствует
+		// Если тип не указан - он будет выведен при инициализации переменной
+		
+		
+		// Приведение типов
+		trace(Std.int(myFloat));
+		trace(Std.string(myFloat));
+
+		// Получение типа переменной
+		trace(Type.typeof(myFloat));
+		trace(Type.typeof(myString));
+		$type(myFloat); // вывод типа переменной при компилляции
+
+		// СТРОКИ
+		// Интерполяция
+		var x:Int = 3;
+		trace('вывод $x и ${x+3}');
+
+		var scc = String.fromCharCode(112); // получение символа по коду
+
+		// MATH
+		Math.random(); // случайное число от 0 до 1, без аргументов
+		Std.random(100); // случайное число от 0 до аргумента (без попадания аргумента в диапазон вывода)
+		
+		
+		// ФУНКЦИИ
+		
+		function myFunc(x:Float = 1, y:Float = 1, ?z:Float):Float {
+			return x * y;
+		}
+		
+		//trace(myFunc(10, 10.2));
+		//trace($type(myFunc));
+		//trace($type(myFunc(2, 2)));
+		$type(myFunc); // без аргументов - вывод сигнатуры функции, с аргументами - вывод типа возвращаемого результата
+		
+		// Анонимные функции
+		var f = function(x, y) {
+			return x + y;
+		}
+		trace(f(2, 3));
+		
+		// Ввод-вывод данных - для neko
+		/*
+		Sys.println("Hello World и Мир!");		
+		trace(Sys.args(), Sys.args()[0]);
+		*/
+		
+		
+		// Файлы - для neko
+		/*
+		import sys.io.File;
+		var s = File.getContent("Main.hx");
+		trace(s);
+		s = "Запись строки в файл";
+		File.saveContent("Main.hx", s)
+		*/
+	}
 }
